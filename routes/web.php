0c16d437b874c9b2a845c71ad0e22204ee73f22c
Juel-11001 <mdjewel20172017@gmail.com>
2025-07-03 05:59:27 +0600

    register the new user

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\V1\TaskController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,6 +13,7 @@ Route::get('/', function () {
 Route::prefix('auth')->group(function (){
     Route::post('/login', LoginController::class);
     Route::post('/logout', LogoutController::class);
+    Route::post('/register', RegisterController::class);
 });
 
 
